3e152dc54b1660e6742fea0ea2e921a5c4f1c833
TimeToStop <evgrat123@mail.ru>
2020-08-30 20:02:53 +0300

    F

--- input.php
+++ input.php
@@ -1,7 +1,33 @@
+
+<?php session_start(); ?>
+<?php
+function isValid()
+{
+    if(isset($_GET['X']) && isset($_GET['Y']) && isset($_GET['R']))
+    {
+        $X = $_GET['X'];
+        $Y = str_ireplace(',','.', $_GET['Y']);
+        $R = str_ireplace(',','.', $_GET['R']);
+        if (!is_numeric($X))
+        {
+            return '<p>X не число!</p>';
+        }
+        elseif (!is_numeric($Y))
+        {
+            return '<p>Y не числo!</p>';
+        }
+        elseif (!is_numeric($R))
+        {
+            return '<p>R не число!</p>';
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta content="text/html; charset=UTF-8"/>
+    <meta content="text/html" charset=UTF-8"/>
     <title>Lab1</title>
     <style type="text/css">
         body {
@@ -31,13 +57,6 @@
 
         input {
             margin: 0 0 0 1px;
-        }
-
-        #answer {
-            background: #5E81A8;
-            width: 60%;
-            table-layout: auto;
-            margin: 10px 0 10px 20%;
         }
 
         .word-break {
@@ -84,7 +103,20 @@
             color: #FF0000;
         }
 
+        .answer {
+            background: #5E81A8;
+            width: 60%;
+            table-layout: auto;
+            margin: 10px 0 10px 20%;
+            border: 1px solid black;
+        }
+
+        .answer td {
+            border: 1px solid black;
+        }
+
     </style>
+
 </head>
 <body>
     <table id="mainTable">
@@ -130,7 +162,7 @@
                                 <td>
                                     <p>
                                         <label for="inputY">Значение Y ∈ (-5;3):</label>
-                                        <input type="text" id="inputY" autocomplete="off" name="Y">
+                                        <input type="text" id="inputY" autocomplete="off" name="Y" value=""/>
                                     </p>
                                 </td>
                             </tr>
@@ -138,15 +170,16 @@
                                 <td>
                                     <p>
                                         <label for="inputR">Значение R ∈ (2;5):</label>
-                                        <input type="text" id="inputR" autocomplete="off" name="R">
+                                        <input type="text" id="inputR" autocomplete="off" name="R" value=""/>
                                     </p>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
                                     <p>
-                                        <input type="submit" value="Отправить">
-                                        <input type="reset" value="Очистить">
+                                        <input type="submit" value="Отправить"/>
+                                        <input type="reset" value="Очистить"/>
+                                        <input type="hidden" name="uniqid" value="<?= uniqid() ?>">
                                     </p>
                                 </td>
                             </tr>
@@ -154,6 +187,15 @@
                                 <td>
                                     <p id="error">
                                     </p>
+
+<?php
+    $err = isValid();
+
+    if(!is_null($err))
+    {
+        echo $err;
+    }
+?>
                                 </td>
                             </tr>
                         </tbody>
@@ -161,30 +203,94 @@
                 </form>
             </td>
         </tr>
-        <?php
-        if(isset($_GET['X']) && isset($_GET['Y']) && isset($_GET['R']))
-        {
-                $X = $_GET['X'];
-                $Y = str_ireplace(",",".", $_GET['Y']);
-                $R = str_ireplace(",",".", $_GET['R']);
-                if (!is_numeric($X))
-                {
-                        echo '<p> X не число!</p>';
-                }
-                elseif (!is_numeric($Y))
-                {
-                        echo '<p> Y не числo!</p>';
-                }
-                elseif (!is_numeric($R))
-                {
-                        echo '<p> R не число!</p>';
-                }
-
-        }
-?>
         <tr>
         <td>
-          
+            <table class="answer">
+                <tbody>
+                    <tr>
+                        <td>X</td>
+                        <td>Y</td>
+                        <td>R</td>
+                        <td>Ответ</td>
+                        <td>Время</td>
+                        <td>Время работы скрипта (в мс)</td>
+                    </tr>
+
+
+                    <?php
+                    $history = (isset($_SESSION['history']) && is_array($_SESSION['history'])) ? $_SESSION['history'] : [];
+                    if (!is_null(isValid())  && isset($_GET['uniqid']))
+                    {
+                        $X = floatval($_GET['X']);
+                        $Y = floatval(str_ireplace(',', '.', $_GET['Y']));
+                        $R = floatval(str_ireplace(',', '.', $_GET['R']));
+                        $IS_SUCCESS = FALSE;
+                        $ANSWER = 'Не попадает в точку';
+
+
+                        if ($X <= 0 && $Y > 0)
+                        {
+                            if ($Y <= $X + ($R / 2))
+                            {
+                                $IS_SUCCESS = TRUE;
+                            }
+                        }
+                        elseif ($X <= 0 && $Y <= 0)
+                        {
+                            $IS_SUCCESS = TRUE;
+                        }
+                        elseif ($X >= 0 && $Y < 0)
+                        {
+                            if ( $X **2 + $Y ** 2 <= $R **2)
+                            {
+                                $IS_SUCCESS = TRUE;
+                            }
+                        }
+
+                        if($IS_SUCCESS)
+                        {
+                            $ANSWER = 'Попадает в точку';
+                        }
+
+
+                        setlocale(LC_ALL, 'ru_RU.UTF-8');
+                        $time = strftime(' %d %b %Y %H:%M:%S', time());
+                        $script = round((microtime(true) - $start) * 10 ** 3, 3);
+                        $script = str_ireplace(",", ".", $script);
+                        $uniqid = $_GET['uniqid'];
+                        if (count($history) == 0 || $history[0]['uniqid'] !== $uniqid)
+                        {
+                            array_unshift($history,
+                                [
+                                    'X' => $X,
+                                    'Y' => $Y,
+                                    'R' => $R,
+                                    'Answer' => $ANSWER,
+                                    'time' => 0,
+                                    'script' => 0,
+                                    'uniqid' => $uniqid
+                                ]);
+                        }
+
+                        $_SESSION['history'] = $history;
+
+                    }
+
+                    foreach ($history as $result) {
+                    ?>
+                    <tr>
+                        <td><?= $result['X'] ?></td>
+                        <td class="word-break"><?= $result['Y'] ?></td>
+                        <td><?= $result['R'] ?></td>
+                        <td><?= $result["Ans"] ?></td>
+                        <td><?= $result["time"] ?></td>
+                        <td class="script_time"><?= $result["script"] ?></td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </td>
     </tr>
     <tr>
@@ -210,6 +316,12 @@
             return false;
         }
 
+        if(inputYElement.value === "")
+        {
+            error.innerText="Введите Y";
+            return false;
+        }
+
         if(y === NaN)
         {
             error.innerText="Введенное значение Y не число";
@@ -224,11 +336,15 @@
             }
         }
 
+        if(inputRElement.value === "")
+        {
+            error.innerText="Введите R";
+            return false;
+        }
 
         if(r === NaN)
         {
             error.innerText="Введенное значение R не число";
-
             return false;
         }
         else
